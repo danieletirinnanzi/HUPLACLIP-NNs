ee24d55c11f76e1b7f1a3a1bd6f0a621644281c4
dtirinna <dtirinna@sissa.it>
2024-05-10 16:55:19 +0200

    jobscript setup on Leonardo

---
+++ scripts/grid_exp_jobscript.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH --mem=32G
+#SBATCH --account=Sis24_piasini       # account name
+#SBATCH --partition=boost_usr_prod # partition name
+#SBATCH --job-name=grid_exp
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=dtirinna@sissa.it
+#SBATCH --output=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.out
+#SBATCH --error=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.err
+
+source $HOME/virtualenvs/dl/bin/activate
+
+cd $HOME/HUPLACLIP-NNs/
+# cd $SLURM_SUBMIT_DIR
+
+srun --unbuffered time python main.py

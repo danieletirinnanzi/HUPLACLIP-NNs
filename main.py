first DDP exp results + fixed bug in losses calculation + synchronization during tests

main.py
@@ -64,7 +64,10 @@ def tests(rank, world_size):
     else:
         print("All tests passed. Proceeding with the experiment.")
     
-    # DDP setup: cleanup after all tests have completed
+    # - making sure processes are synchronized on all devices
+    torch.distributed.barrier()
+    
+    # DDP setup: cleanup after all tests have completed  
     cleanup_DDP()
 
 # Defining full experiment:
@@ -72,6 +75,9 @@ def full_exp(rank, world_size):
     
     print(f"Running full experiment on rank {rank}.")
     setup_DDP(rank, world_size)    
+
+    # - making sure processes are synchronized on all devices
+    torch.distributed.barrier()
 
     # storing starting time of the experiment in string format:
     start_time = datetime.datetime.now()

src/train_test.py
@@ -355,7 +355,7 @@ def train_model(
                     # Aggregate validation loss across GPUs:
                     stdval_loss_tensor = torch.tensor(stdval_loss.item(), device=rank)
                     torch.distributed.all_reduce(stdval_loss_tensor, op=torch.distributed.ReduceOp.SUM) 
-                    global_stdval_loss = stdval_loss_tensor.item() / training_parameters["num_val"]
+                    global_stdval_loss = stdval_loss_tensor.item() / world_size
                     # Check early stopping condition:
                     early_stop = early_stopper.should_stop(global_stdval_loss)
                     
@@ -403,7 +403,7 @@ def train_model(
 
                         # updating dictionary with validation losses for all task versions:
                         if rank == 0:
-                            global_val_loss = val_loss_tensor.item() / training_parameters["num_val"]
+                            global_val_loss = val_loss_tensor.item() / world_size
                             complete_val_dict[f"val-loss-{current_clique_size_val}"] = (
                                 global_val_loss
                             )
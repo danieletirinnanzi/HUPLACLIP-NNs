a3c41deda867e14765017aa3149a68448af91a7a
dtirinna <dtirinna@sissa.it>
2024-12-04 17:20:27 +0100

    launched MLP-VIT + CNN exps

--- scripts/cnn_exp_jobscript.sh
+++ scripts/cnn_exp_jobscript.sh
@@ -2,21 +2,21 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=23:55:00
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=32
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128G
 #SBATCH --account=Sis24_piasini       # account name
 #SBATCH --partition=boost_usr_prod # partition name
-#SBATCH --job-name=grid_exp
+#SBATCH --job-name=cnn_exp
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dtirinna@sissa.it
-#SBATCH --output=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/cnn_exp/%x.%j.out
-#SBATCH --error=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/cnn_exp/%x.%j.err
+#SBATCH --output=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.out
+#SBATCH --error=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.err
 
 source $HOME/virtualenvs/dl/bin/activate
 
 cd $HOME/HUPLACLIP-NNs/
 # cd $SLURM_SUBMIT_DIR
 
-srun --unbuffered time python main.py
+torchrun --standalone --nproc_per_node=4 main.py
--- scripts/mlp-vit_exp_jobscript.sh
+++ scripts/mlp-vit_exp_jobscript.sh
@@ -2,17 +2,17 @@
 
 #SBATCH --nodes=1
 #SBATCH --time=23:55:00
-#SBATCH --ntasks=4
-#SBATCH --cpus-per-task=32
+#SBATCH --ntasks-per-node=4
+#SBATCH --cpus-per-task=8
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128G
 #SBATCH --account=Sis24_piasini       # account name
 #SBATCH --partition=boost_usr_prod # partition name
-#SBATCH --job-name=grid_exp
+#SBATCH --job-name=mlp-vit_exp
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=dtirinna@sissa.it
-#SBATCH --output=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/mlp-vit_exp/%x.%j.out
-#SBATCH --error=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/mlp-vit_exp/%x.%j.err
+#SBATCH --output=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.out
+#SBATCH --error=/leonardo/home/userexternal/dtirinna/HUPLACLIP-NNs/out/%x.%j.err
 
 source $HOME/virtualenvs/dl/bin/activate
 

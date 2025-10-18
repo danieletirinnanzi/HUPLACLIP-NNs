061b643ba180633f5d2f2ca97740f8f619fdc110
dtirinna <dtirinna@sissa.it>
2025-10-18 16:04:48 +0200

    uploading results for CNN at N=100 (new pipeline)

--- jobscripts/cnn_exp_jobscript.sh
+++ jobscripts/cnn_exp_jobscript.sh
@@ -22,9 +22,9 @@ cd $HOME/HUPLACLIP-NNs/
 # --------- HOW TO RUN THIS SCRIPT (uncomment the line corresponding to the experiment to be run) ---------
 # NOTE: in both cases, the configuration file in "test_models.py" (line 16) must match the one used here
 # - STANDARD CASE:
-torchrun --standalone --nproc_per_node=4 main.py --config docs/cnn_exp_config.yml
+# torchrun --standalone --nproc_per_node=4 main.py --config docs/cnn_exp_config.yml
 # - RESUME CASE (for continuing a previous experiment):
-# torchrun --standalone --nproc_per_node=4 main.py --resume --exp_name cnn_exp_2025-10-...
+torchrun --standalone --nproc_per_node=4 main.py --resume --exp_name cnn_exp_2025-10-13_13-54-52
 # - TESTING TRAINED MODEL:
 # export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True # to avoid CUDA out of memory error
 # torchrun --standalone --nproc_per_node=4 -m tests.test_loop.testloop_test_DDP
